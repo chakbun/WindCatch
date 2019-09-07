b96f30cf81044468389a225db0fc3b3876358d25
Jaben <cheungjaben@hotmail.com>
2019-09-07 11:22:26 +0800

    add tableview to show typhoon information

--- WindCatch/Controller/ViewController.swift
+++ WindCatch/Controller/ViewController.swift
@@ -9,16 +9,35 @@
 import UIKit
 import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var typhoonList: [WCTyphoon]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        weak var weakSelf = self
         WCHttpRequestManager.shareManager.listTyphoonWith { (typhoons, error) in
             ZBLog(typhoons)
+            weakSelf?.typhoonList = typhoons
+            weakSelf?.tableView.reloadData()
         }
     }
 
+}
 
+// MARK: - TableView Data Source
+extension ViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return typhoonList?.count ?? 0
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let typhoonModel = self.typhoonList?[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WCTyphoonListCell", for: indexPath)
+        let nameLabel: UILabel = cell.viewWithTag(1) as! UILabel
+        nameLabel.text = typhoonModel?.chineseName ?? "Unknow"
+        return cell
+    }
 }
 
