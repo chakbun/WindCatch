a3812c629c573bfcc6566c8b8ae6e77054f762e5
Jaben <cheungjaben@hotmail.com>
2019-09-09 23:08:18 +0800

    add line

--- WindCatch/Controller/WCMapController.swift
+++ WindCatch/Controller/WCMapController.swift
@@ -18,29 +18,25 @@ class WCMapController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tpMapView.delegate = self
-        
         weak var weakSelf = self
         
         if let typhoon = typhoon {
             WCHttpRequestManager.shareManager.loadTyphoonDetailWith(id: typhoon.id) { (typhoon, error) in
                 if let coordinateArray = typhoon?.coordinateArray {
                     let polyLine = MKPolyline.init(coordinates: coordinateArray, count: coordinateArray.count)
-                    weakSelf?.tpMapView.centerCoordinate = coordinateArray[0]
+                    weakSelf?.tpMapView.setRegion(MKCoordinateRegion.init(center: coordinateArray.last!, span: MKCoordinateSpan.init(latitudeDelta: 10, longitudeDelta: 10)), animated: true)
                     weakSelf?.tpMapView.addOverlay(polyLine)
                 }
             }
         }
     }
-
 }
 
 extension WCMapController: MKMapViewDelegate {
-    
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         let render = MKPolylineRenderer.init(overlay: overlay)
         render.lineWidth = 2.0
         render.strokeColor = .red
-        render.lineDashPattern = [1, 2, 1]
         return render
     }
     
