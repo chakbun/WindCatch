542a22df43dc8717845474585dbc38573feacf16
chakbun <cheungjaben@hotmail.com>
2019-09-26 21:39:31 +0800

    add color index style from storyboard

--- WindCatch/Utils/Weather.swift
+++ WindCatch/Utils/Weather.swift
@@ -18,13 +18,13 @@ struct Weather {
     var color: UIColor {
         switch colorMsg {
         case "红色":
-            return UIColor(red: 198/255.0, green: 60/255.0, blue: 47/255.0, alpha: 1.0)
+            return UIColor(red: 198/255.0, green: 63/255.0, blue: 50/255.0, alpha: 1.0)
         case "黄色":
-            return UIColor(red: 246/255.0, green: 191/255.0, blue: 80/255.0, alpha: 1.0)
+            return UIColor(red: 248/255.0, green: 236/255.0, blue: 87/255.0, alpha: 1.0)
         case "橙色":
-            return UIColor(red: 241/255.0, green: 157/255.0, blue: 56/255.0, alpha: 1.0)
+            return UIColor(red: 235/255.0, green: 144/255.0, blue: 61/255.0, alpha: 1.0)
         case "蓝色":
-            return UIColor(red: 64/255.0, green: 103/255.0, blue: 236/255.0, alpha: 1.0)
+            return UIColor(red: 59/255.0, green: 105/255.0, blue: 245/255.0, alpha: 1.0)
         default:
             return .black
         }
