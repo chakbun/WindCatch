c4ff3ef65bbebf40be12f06b267b599d8714f564
chakbun <cheungjaben@hotmail.com>
2019-09-28 21:36:10 +0800

    add warning detail story board

---
+++ WindCatch/Controller/WarningDetailController.swift
@@ -0,0 +1,17 @@
+//
+//  WarningDetailController.swift
+//  WindCatch
+//
+//  Created by jaben on 2019/9/28.
+//  Copyright © 2019 Jaben. All rights reserved.
+//
+
+import UIKit
+
+class WarningDetailController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
